01355e4e9fafd419f405c67d63132af92a39aa1b
Kayk el <kayksantana133@gmail.com>
2026-01-07 21:59:16 -0300

    feat(reservation): add cancellation rules and user reservations query

--- src/main/java/com/reservation/room_reservation_api/service/ReservationService.java
+++ src/main/java/com/reservation/room_reservation_api/service/ReservationService.java
@@ -6,14 +6,21 @@ import com.reservation.room_reservation_api.domain.Room;
 import com.reservation.room_reservation_api.domain.User;
 import com.reservation.room_reservation_api.dto.request.ReservationRequestDTO;
 import com.reservation.room_reservation_api.dto.response.ReservationResponseDTO;
+import com.reservation.room_reservation_api.exception.BusinessException;
+import com.reservation.room_reservation_api.exception.ResourceNotFoundException;
 import com.reservation.room_reservation_api.mapper.ReservationMapper;
 import com.reservation.room_reservation_api.repository.ReservationRepository;
+import com.reservation.room_reservation_api.repository.UserRepository;
 import com.reservation.room_reservation_api.service.validation.ReservationValidator;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
 
 
 @Service
@@ -24,6 +31,7 @@ public class ReservationService {
     private final UserService userService;
     private final RoomService roomService;
     private final ReservationValidator validator;
+    private final UserRepository userRepository;
 
     @Transactional
     public ReservationResponseDTO createReservation(ReservationRequestDTO dto) {
@@ -47,4 +55,90 @@ public class ReservationService {
                 reservationRepository.save(reservation)
         );
     }
+
+
+
+    public ReservationResponseDTO confirmReservation(Long reservationId){
+
+        Reservation reservation = reservationRepository.findById(reservationId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Reservation not found with id: " + reservationId
+                        )
+                );
+
+        if (reservation.getStatus() != ReservationStatus.PENDING){
+            throw new BusinessException(
+                    "Only PENDING reservations can be confirmed"
+            );
+        }
+
+        validator.validate(
+                reservation.getRoom(),
+                reservation.getStartDateTime(),
+                reservation.getDurationInHours()
+        );
+
+        reservation.setStatus(ReservationStatus.CONFIRMED);
+
+        return ReservationMapper.toDTO(
+                reservationRepository.save(reservation)
+        );
+
+    }
+
+
+
+    public ReservationResponseDTO cancelReservation(Long reservationId){
+
+        Reservation reservation = reservationRepository.findById(reservationId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "Reservation not found with id: " + reservationId
+                        )
+                );
+
+        if (reservation.getStatus() == ReservationStatus.CANCELLED
+                || reservation.getStatus() == ReservationStatus.COMPLETED) {
+            throw new BusinessException(
+                    "Cancelled or completed reservations cannot be confirmed"
+            );
+        }
+
+
+        LocalDateTime minimumAllowedTime = LocalDateTime.now().plusHours(24);
+
+        if (reservation.getStartDateTime().isBefore(minimumAllowedTime)) {
+            throw new BusinessException(
+                    "Reservations can only be cancelled at least 24 hours in advance"
+            );
+    }
+
+        reservation.setStatus(ReservationStatus.CANCELLED);
+
+        return ReservationMapper.toDTO(
+                reservationRepository.save(reservation)
+        );
+}
+
+
+    public List<ReservationResponseDTO> getReservationsByUser(UUID userId){
+        User user = userRepository.findById(userId)
+                .orElseThrow(() ->
+                        new ResourceNotFoundException(
+                                "User not found with UUID: " + userId
+                        )
+                );
+
+        List<Reservation> reservations =
+                reservationRepository.findByUserAndStatus(
+                        user,
+                        ReservationStatus.CONFIRMED
+                );
+
+        return reservations.stream()
+                .map(ReservationMapper::toDTO)
+                .toList();
+    }
+
 }
