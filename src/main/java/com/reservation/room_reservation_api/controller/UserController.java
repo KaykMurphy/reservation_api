ba138b930bc78d023ba611a187941f89d6eda271
Kayk el <kayksantana133@gmail.com>
2026-01-07 22:14:04 -0300

    feat: add UserController with user creation and listing endpoints

---
+++ src/main/java/com/reservation/room_reservation_api/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.reservation.room_reservation_api.controller;
+
+import com.reservation.room_reservation_api.domain.User;
+import com.reservation.room_reservation_api.dto.request.UserRequestDTO;
+import com.reservation.room_reservation_api.dto.response.UserResponseDTO;
+import com.reservation.room_reservation_api.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.function.EntityResponse;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<UserResponseDTO> createUser(
+            @RequestBody @Valid UserRequestDTO dto) {
+
+        UserResponseDTO response = userService.createUser(dto);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
+
+        List<UserResponseDTO> response = userService.getAllUsers();
+
+        return ResponseEntity
+                .ok(response);
+    }
+}
+
